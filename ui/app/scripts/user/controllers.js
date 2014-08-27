ad5e8afd1f7cfb77a9b88b86876f455cc7098fba
Grisha Pomadchin <gr.pomadchin@gmail.com>
2014-08-27 22:45:26 +0400

    checkboxes erasure fix

--- ui/app/scripts/user/controllers.js
+++ ui/app/scripts/user/controllers.js
@@ -70,6 +70,40 @@ define(['angular', 'jquery'], function(angular, $) {
                 //$location.path('/houses').replace();
             });
 
+        $scope.clone = function (obj) {
+            var copy;
+
+            // Handle the 3 simple types, and null or undefined
+            if (null == obj || "object" != typeof obj) return obj;
+
+            // Handle Date
+            if (obj instanceof Date) {
+                copy = new Date();
+                copy.setTime(obj.getTime());
+                return copy;
+            }
+
+            // Handle Array
+            if (obj instanceof Array) {
+                copy = [];
+                for (var i = 0, len = obj.length; i < len; i++) {
+                    copy[i] = $scope.clone(obj[i]);
+                }
+                return copy;
+            }
+
+            // Handle Object
+            if (obj instanceof Object) {
+                copy = {};
+                for (var attr in obj) {
+                    if (obj.hasOwnProperty(attr)) copy[attr] = $scope.clone(obj[attr]);
+                }
+                return copy;
+            }
+
+            throw new Error("Unable to copy obj! Its type isn't supported.");
+        };
+
         $scope.toggleSelection = function toggleSelection(p) {
             var idx = $scope.user.privacy.map(function(e) { return e.name; }).indexOf(p);
 
@@ -86,11 +120,11 @@ define(['angular', 'jquery'], function(angular, $) {
         };
 
         $scope.save = function(generalInfo) {
-            generalInfo.privacy = $scope.clone($scope.user.privacy).filter(function(e) { return (e.selected === true); }).map(function(e) { return e.name; });
-            console.log(generalInfo);
-            console.log($scope.user.privacy);
+            var sendData = $scope.clone(generalInfo);
+            sendData.privacy = $scope.user.privacy.filter(function(e) { return (e.selected === true); }).map(function(e) { return e.name; });
+            console.log(sendData);
 
-            userService.saveAbout(generalInfo);
+            userService.saveAbout(sendData);
         }
 
     };
