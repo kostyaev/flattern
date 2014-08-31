f633030faa2cfeb9ddac56d2ef8a35a440e280db
Grisha Pomadchin <gr.pomadchin@gmail.com>
2014-08-31 12:30:54 +0400

    *code tyle fixes (user, auth); *enable auth

--- ui/app/scripts/user/main.js
+++ ui/app/scripts/user/main.js
@@ -2,7 +2,7 @@
  * User package module.
  * Manages all sub-modules so other RequireJS modules only have to import the package.
  */
-define(['angular', './routes', './controllers', './services'], function(angular, routes, controllers) {
+define(['angular', './routes', './controllers', './services'], function(angular, routes) {
     'use strict';
 
     var mod = angular.module('flattern.user', ['ui.router', 'ui.bootstrap', 'ngSanitize', 'ui.select', 'user.routes', 'user.services']);
