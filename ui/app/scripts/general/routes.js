d8240b3fb3df259e79680bc96b8cf828d01c0719
virtuozzo <virtuozz.me@gmail.com>
2014-09-04 23:40:12 +0400

    refactor html files, bind html to ui routes

--- ui/app/scripts/general/routes.js
+++ ui/app/scripts/general/routes.js
@@ -6,6 +6,22 @@ define(['angular', './controllers'], function(angular, controllers) {
     var mod = angular.module('general.routes', []);
     mod.config(['$stateProvider',
         function($stateProvider)  {
+            $stateProvider
+                .state('main', {
+                    abstract: true,
+                    views: {
+                        '': {
+                            templateUrl: 'views/general/main.html'
+                        },
+                        'header@main': {
+                            templateUrl: 'views/general/header.html'
+                        },
+                        'footer@main': {
+                            templateUrl: 'views/general/footer.html'
+                        }
+                    }
+                })
+
 
         }]);
     return mod;
