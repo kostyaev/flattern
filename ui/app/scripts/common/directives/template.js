eb726e68b41be2e477d69c92ab2056662b91313b
Grisha Pomadchin <gr.pomadchin@gmail.com>
2014-09-06 11:14:47 +0400

    +directives; *not removed helper.js; strange horizontall search

--- ui/app/scripts/common/directives/template.js
+++ ui/app/scripts/common/directives/template.js
@@ -1,19 +1,69 @@
 define(['angular', '../services/helper'], function(angular) {
     var mod = angular.module('common.directives.template', ['common.helper']);
+    var $ = angular.element;
     mod.directive('customInit', function (custom) {
-        var $ = angular.element;
 
         return {
             restrict: 'A',
             link: function (scope, element, attrs) {
+                var $ = element;
                 console.log('customInit');
-                custom.equalHeight('.equal-height');
+
+                //  Equal heights
+
+                function equalHeight(container) {
+
+                    var currentTallest = 0,
+                        currentRowStart = 0,
+                        rowDivs = new Array(),
+                        $el,
+                        topPosition = 0;
+                    $(container).each(function() {
+
+                        $el = $(this);
+                        $($el).height('auto');
+                        topPostion = $el.position().top;
+
+                        if (currentRowStart != topPostion) {
+                            for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+                                rowDivs[currentDiv].height(currentTallest);
+                            }
+                            rowDivs.length = 0; // empty the array
+                            currentRowStart = topPostion;
+                            currentTallest = $el.height();
+                            rowDivs.push($el);
+                        } else {
+                            rowDivs.push($el);
+                            currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
+                        }
+                        for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+                            rowDivs[currentDiv].height(currentTallest);
+                        }
+                    });
+                }
+
+                function setNavigationPosition(){
+                    $('.nav > li').each(function () {
+                        if($(this).hasClass('has-child')){
+                            var fullNavigationWidth = $(this).children('.child-navigation').width() + $(this).children('.child-navigation').children('li').children('.child-navigation').width();
+                            if(($(this).children('.child-navigation').offset().left + fullNavigationWidth) > $(window).width()){
+                                $(this).children('.child-navigation').addClass('navigation-to-left');
+                            }
+                        }
+                    });
+                }
+
+                function setCarouselWidth(){
+                    $('.carousel-full-width').css('width', $(window).width());
+                }
+
+                equalHeight('.equal-height');
 
                 $('.nav > li > ul li > ul').css('left', $('.nav > li > ul').width());
 
                 var navigationLi = $('.nav > li');
                 navigationLi.hover(function() {
-                    if ($('body').hasClass('navigation-fixed-bottom')){
+                    if ($('content').hasClass('navigation-fixed-bottom')){
                         if ($(window).width() > 768) {
                             var spaceUnderNavigation = $(window).height() - ($(this).offset().top - $(window).scrollTop());
                             if(spaceUnderNavigation < $(this).children('.child-navigation').height()){
@@ -27,7 +77,7 @@ define(['angular', '../services/helper'], function(angular) {
                     }
                 });
 
-                custom.setNavigationPosition();
+                setNavigationPosition();
 
                 $('.tool-tip').tooltip();
 
@@ -81,8 +131,8 @@ define(['angular', '../services/helper'], function(angular) {
 
                 //  Fit videos width and height
 
-                if($(".video").length > 0) {
-                    $(".video").fitVids();
+                if($('.video').length > 0) {
+                    $('.video').fitVids();
                 }
 
                 //  Price slider
@@ -106,7 +156,7 @@ define(['angular', '../services/helper'], function(angular) {
                 $(window).scroll(function () {
                     var scrollAmount = $(window).scrollTop() / 1.5;
                     scrollAmount = Math.round(scrollAmount);
-                    if ( $("body").hasClass("navigation-fixed-bottom") ) {
+                    if ( $('body').hasClass('navigation-fixed-bottom') ) {
                         if ($(window).scrollTop() > $(window).height() - $('.navigation').height() ) {
                             $('.navigation').addClass('navigation-fix-to-top');
                         } else {
@@ -133,11 +183,11 @@ define(['angular', '../services/helper'], function(angular) {
                     var target = this.hash,
                         $target = $(target);
                     if ($(window).width() > 768) {
-                        $('html, body').stop().animate({
+                        $('html, content').stop().animate({
                             'scrollTop': $target.offset().top - $('.navigation').height()
                         }, 2000)
                     } else {
-                        $('html, body').stop().animate({
+                        $('html, content').stop().animate({
                             'scrollTop': $target.offset().top
                         }, 2000)
                     }
@@ -176,7 +226,7 @@ define(['angular', '../services/helper'], function(angular) {
                         targetType : 'number',
                         targetFormat : 'Rating: {score}',
                         click: function(score, evt) {
-                            custom.showRatingForm();
+                            showRatingForm();
                         }
                     });
                 }
@@ -184,12 +234,12 @@ define(['angular', '../services/helper'], function(angular) {
                 //  Agent State
 
                 $('#agent-switch').on('ifClicked', function(event) {
-                    custom.agentState();
+                    agentState();
                 });
 
                 $('#create-account-user').on('ifClicked', function(event) {
                     $('#agent-switch').data('agent-state', '');
-                    custom.agentState();
+                    agentState();
                 });
 
                 // Set Bookmark button attribute
@@ -212,7 +262,7 @@ define(['angular', '../services/helper'], function(angular) {
                     }
                 });
 
-                if ($('body').hasClass('navigation-fixed-bottom')){
+                if ($('content').hasClass('navigation-fixed-bottom')){
                     $('#page-content').css('padding-top',$('.navigation').height());
                 }
 
@@ -291,7 +341,9 @@ define(['angular', '../services/helper'], function(angular) {
                     );
                 }
 
-                custom.centerSearchBox();
+                centerSearchBox();
+
+
 
             }
         };
@@ -305,11 +357,11 @@ define(['angular', '../services/helper'], function(angular) {
     });
 
 
-    mod.directive("footerThumbnails", function() {
+    mod.directive('footerThumbnails', function() {
         return {
-            restrict: "AE",
+            restrict: 'AE',
             link: function(scope, el, attrs) {
-                console.log("activation thumbnails");
+                console.log('activation thumbnails');
                 var $ = angular.element;
                 var i = 0;
                 var rows = 1; // how many rows to display, default = 1
@@ -351,6 +403,9 @@ define(['angular', '../services/helper'], function(angular) {
             link: function(scope, el, attrs) {
                 console.log("activating map");
                 var $ = angular.element;
+                $('#map .marker-style').css('opacity', '.5 !important');
+                $('#map .marker-style').css('bakground-color', 'red');
+                $('.search-box.map').addClass('show-search-box');
                 var $content = $('content');
                 if($content.hasClass('has-fullscreen-map')) {
                     el.height($(window).height() - $('.navigation').height());
@@ -369,6 +424,173 @@ define(['angular', '../services/helper'], function(angular) {
                 }
             }
         };
+    });
+
+    mod.directive('centerSlider', function () {
+        return {
+            // Restrict it to be an attribute in this case
+            restrict: 'AE',
+            // responsible for registering DOM listeners as well as updating the DOM
+            link: function(scope, el, attrs) {
+                console.log('centerSlider');
+                var $ = angular.element;
+                if ($(window).width() < 979) {
+                    var $navigation = $('.navigation');
+                    $('#slider .slide').height($(window).height() - $navigation.height());
+                    $('#slider').height($(window).height() - $navigation.height());
+
+                }
+                var imageWidth = $('#slider .slide img').width();
+                var viewPortWidth = $(window).width();
+                var centerImage = ( imageWidth/2 ) - ( viewPortWidth/2 );
+                $('#slider .slide img').css('left', -centerImage);
+            }
+        }
+    });
+
+    mod.directive('agentState', function () {
+        return {
+            // Restrict it to be an attribute in this case
+            restrict: 'AE',
+            // responsible for registering DOM listeners as well as updating the DOM
+            link: function(scope, el, attrs) {
+                var _originalHeight = $('#agency .form-group').height();
+                var $agentSwitch = $('#agent-switch');
+                var $agency = $('#agency');
+
+                if ($agentSwitch.data('agent-state') == 'is-agent') {
+                    $agentSwitch.iCheck('check');
+                    $agency.removeClass('disabled');
+                    $agency.addClass('enabled');
+                    $agentSwitch.data('agent-state', '');
+                } else {
+                    $agentSwitch.data('agent-state', 'is-agent');
+                    $agency.removeClass('enabled');
+                    $agency.addClass('disabled');
+                }
+            }
+        }
+    });
+
+    mod.directive('centerSearchBox', function () {
+        return {
+            // Restrict it to be an attribute in this case
+            restrict: 'AE',
+            // responsible for registering DOM listeners as well as updating the DOM
+            link: function(scope, el, attrs) {
+                var $searchBox = $('.search-box-wrapper');
+                var $navigation = $('.navigation');
+                var positionFromBottom = 20;
+                if ($('content').hasClass('navigation-fixed-top')) {
+                    $('#map, #slider').css('margin-top',$navigation.height());
+                    $searchBox.css('z-index',98);
+                } else {
+                    $('.leaflet-map-pane').css('top',-50);
+                    $(".homepage-slider").css('margin-top', -$('.navigation header').height());
+                }
+                if ($(window).width() > 768) {
+                    $('#slider .slide .overlay').css('margin-bottom',$navigation.height());
+                    $('#map, #slider').each(function () {
+                        if (!$('content').hasClass('horizontal-search-float')) {
+                            var mapHeight = $(this).height();
+                            var contentHeight = $('.search-box').height();
+                            var top;
+                            if($('content').hasClass('has-fullscreen-map')) {
+                                top = (mapHeight / 2) - (contentHeight / 2);
+                            }
+                            else {
+                                top = (mapHeight / 2) - (contentHeight / 2) + $('.navigation').height();
+                            }
+                            $('.search-box-wrapper').css('top', top);
+                        } else {
+                            $searchBox.css('top', $(this).height() + $navigation.height() - $searchBox.height() - positionFromBottom);
+                            $('#slider .slide .overlay').css('margin-bottom',$navigation.height() + $searchBox.height() + positionFromBottom);
+                            if ($('content').hasClass('has-fullscreen-map')) {
+                                $('.search-box-wrapper').css('top', $(this).height() - $('.navigation').height());
+                            }
+                        }
+                    });
+                }
+            }
+        }
+    });
+
+    mod.directive('owlCarousel', function() {
+        return {
+            // Restrict it to be an attribute in this case
+            restrict: 'AE',
+            // responsible for registering DOM listeners as well as updating the DOM
+            link: function(scope, el, attrs) {
+
+                function setCarouselWidth() {
+                    $('.carousel-full-width').css('width', $(window).width());
+                }
+
+                // Disable click when dragging
+                function disableClick(){
+                    $('.owl-carousel .property').css('pointer-events', 'none');
+                }
+                // Enable click after dragging
+                function enableClick(){
+                    $('.owl-carousel .property').css('pointer-events', 'auto');
+                }
+
+                if ($('.owl-carousel').length > 0) {
+                    if ($('.carousel-full-width').length > 0) {
+                        setCarouselWidth();
+                    }
+                    $(".featured-properties-carousel").owlCarousel({
+                        items: 5,
+                        itemsDesktop: [1700,4],
+                        responsiveBaseWidth: ".featured-properties-carousel",
+                        pagination: false,
+                        startDragging: disableClick,
+                        beforeMove: enableClick
+                    });
+                    $(".testimonials-carousel").owlCarousel({
+                        items: 1,
+                        responsiveBaseWidth: ".testimonial",
+                        pagination: true
+                    });
+                    $(".property-carousel").owlCarousel({
+                        items: 1,
+                        responsiveBaseWidth: ".property-slide",
+                        pagination: false,
+                        autoHeight : true,
+                        navigation: true,
+                        navigationText: ["",""],
+                        startDragging: disableClick,
+                        beforeMove: enableClick
+                    });
+                    $(".homepage-slider").owlCarousel({
+                        autoPlay: 10000,
+                        navigation: true,
+                        mouseDrag: false,
+                        items: 1,
+                        responsiveBaseWidth: ".slide",
+                        pagination: false,
+                        transitionStyle : 'fade',
+                        navigationText: ["",""],
+                        afterInit: sliderLoaded,
+                        afterAction: animateDescription,
+                        startDragging: animateDescription
+                    });
+                }
+                function sliderLoaded(){
+                    $('#slider').removeClass('loading');
+                    document.getElementById("loading-icon").remove();
+                    centerSlider();
+                }
+                function animateDescription(){
+                    var $description = $(".slide .overlay .info");
+                    $description.addClass('animate-description-out');
+                    $description.removeClass('animate-description-in');
+                    setTimeout(function() {
+                        $description.addClass('animate-description-in');
+                    }, 400);
+                }
+            }
+        }
     });
 
     return mod;
--- ui/app/scripts/common/services/helper.js
+++ ui/app/scripts/common/services/helper.js
@@ -13,60 +13,8 @@ define(['angular'], function(angular) {
         };
     });
 
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    // Functions
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
     mod.service('custom', function () {
         var $ = angular.element;
-
-        var centerSlider = function () {
-            if ($(window).width() < 979) {
-                var $navigation = $('.navigation');
-                $('#slider .slide').height($(window).height() - $navigation.height());
-                $('#slider').height($(window).height() - $navigation.height());
-
-            }
-            var imageWidth = $('#slider .slide img').width();
-            var viewPortWidth = $(window).width();
-            var centerImage = ( imageWidth/2 ) - ( viewPortWidth/2 );
-            $('#slider .slide img').css('left', -centerImage);
-        };
-
-        var setNavigationPosition = function () {
-            $('.nav > li').each(function () {
-                if($(this).hasClass('has-child')) {
-                    var fullNavigationWidth = $(this).children('.child-navigation').width() + $(this).children('.child-navigation').children('li').children('.child-navigation').width();
-                    if(($(this).children('.child-navigation').offset().left + fullNavigationWidth) > $(window).width()) {
-                        $(this).children('.child-navigation').addClass('navigation-to-left');
-                    }
-                }
-            })
-        };
-
-        var agentState = function () {
-            var _originalHeight = $('#agency .form-group').height();
-            var $agentSwitch = $('#agent-switch');
-            var $agency = $('#agency');
-
-            if ($agentSwitch.data('agent-state') == 'is-agent') {
-                $agentSwitch.iCheck('check');
-                $agency.removeClass('disabled');
-                $agency.addClass('enabled');
-                $agentSwitch.data('agent-state', '');
-            } else {
-                $agentSwitch.data('agent-state', 'is-agent');
-                $agency.removeClass('enabled');
-                $agency.addClass('disabled');
-            }
-        };
-
-        var initCounter = function () {
-            $('.number').countTo({
-                speed: 3000,
-                refreshInterval: 50
-            });
-        };
 
         var showAllButton = function () {
             var rowsToShow = 2; // number of collapsed rows to show
@@ -87,58 +35,35 @@ define(['angular'], function(angular) {
 
         };
 
-        var centerSearchBox = function () {
-            var $searchBox = $('.search-box-wrapper');
-            var $navigation = $('.navigation');
-            var positionFromBottom = 20;
-            if ($('body').hasClass('navigation-fixed-top')) {
-                $('#map, #slider').css('margin-top',$navigation.height());
-                $searchBox.css('z-index',98);
-            } else {
-                $('.leaflet-map-pane').css('top',-50);
-                $(".homepage-slider").css('margin-top', -$('.navigation header').height());
-            }
-            if ($(window).width() > 768) {
-                $('#slider .slide .overlay').css('margin-bottom',$navigation.height());
-                $('#map, #slider').each(function () {
-                    if (!$('body').hasClass('horizontal-search-float')) {
-                        var mapHeight = $(this).height();
-                        var contentHeight = $('.search-box').height();
-                        var top;
-                        if($('body').hasClass('has-fullscreen-map')) {
-                            top = (mapHeight / 2) - (contentHeight / 2);
-                        }
-                        else {
-                            top = (mapHeight / 2) - (contentHeight / 2) + $('.navigation').height();
-                        }
-                        $('.search-box-wrapper').css('top', top);
-                    } else {
-                        $searchBox.css('top', $(this).height() + $navigation.height() - $searchBox.height() - positionFromBottom);
-                        $('#slider .slide .overlay').css('margin-bottom',$navigation.height() + $searchBox.height() + positionFromBottom);
-                        if ($('body').hasClass('has-fullscreen-map')) {
-                            $('.search-box-wrapper').css('top', $(this).height() - $('.navigation').height());
-                        }
+        var initCounter = function () {
+            $('.number').countTo({
+                speed: 3000,
+                refreshInterval: 50
+            });
+        };
+
+        var setNavigationPosition = function () {
+            $('.nav > li').each(function () {
+                if($(this).hasClass('has-child')){
+                    var fullNavigationWidth = $(this).children('.child-navigation').width() + $(this).children('.child-navigation').children('li').children('.child-navigation').width();
+                    if(($(this).children('.child-navigation').offset().left + fullNavigationWidth) > $(window).width()){
+                        $(this).children('.child-navigation').addClass('navigation-to-left');
                     }
-                });
-            }
+                }
+            });
         };
 
         var setCarouselWidth = function () {
             $('.carousel-full-width').css('width', $(window).width());
         };
 
-        var showRatingForm = function () {
-            $('.rating-form').css('height', $('.rating-form form').height() + 85 + 'px');
-        };
-
-        var equalHeight = function (container) {
+        var equalHeight = function(container) {
 
             var currentTallest = 0,
                 currentRowStart = 0,
                 rowDivs = new Array(),
                 $el,
                 topPosition = 0;
-
             $(container).each(function() {
 
                 $el = $(this);
@@ -163,131 +88,45 @@ define(['angular'], function(angular) {
             });
         };
 
-        // Disable click when dragging
-        var disableClick = function () {
-            $('.owl-carousel .property').css('pointer-events', 'none');
-        };
-        // Enable click after dragging
-        var enableClick = function () {
-            $('.owl-carousel .property').css('pointer-events', 'auto');
-        };
+        var centerSlider = function() {
+            if ($(window).width() < 979) {
+                var $navigation = $('.navigation');
+                $('#slider .slide').height($(window).height() - $navigation.height());
+                $('#slider').height($(window).height() - $navigation.height());
 
-        var sliderLoaded = function () {
-            $('#slider').removeClass('loading');
-            document.getElementById("loading-icon").remove();
-            centerSlider();
-        };
-
-        var animateDescription = function () {
-            var $description = $(".slide .overlay .info");
-            $description.addClass('animate-description-out');
-            $description.removeClass('animate-description-in');
-            setTimeout(function() {
-                $description.addClass('animate-description-in');
-            }, 400);
+            }
+            var imageWidth = $('#slider .slide img').width();
+            var viewPortWidth = $(window).width();
+            var centerImage = ( imageWidth/2 ) - ( viewPortWidth/2 );
+            $('#slider .slide img').css('left', -centerImage);
         };
 
         var onStateChange = function () {
-
-            //  Show Search Box on Map
-            $('.search-box.map').addClass('show-search-box');
-
-            //  Show All button
-
             this.showAllButton();
-
-            //  Draw thumbnails in the footer
-
-            //this.drawFooterThumbnails();
-
-            //  Show counter after appear
 
             var $number = $('.number');
             if ($number.length > 0 ) {
                 $number.waypoint(function() {
-                    initCounter();
+                    this.initCounter();
                 }, { offset: '100%' });
             }
 
-            agentState();
+            $(window).on('resize', function(){
+                this.setNavigationPosition();
+                this.setCarouselWidth();
+                this.equalHeight('.equal-height');
+                this.centerSlider();
+            });
 
-            //  Owl Carousel
-
-            if ($('.owl-carousel').length > 0) {
-                if ($('.carousel-full-width').length > 0) {
-                    setCarouselWidth();
-                }
-                $(".featured-properties-carousel").owlCarousel({
-                    items: 5,
-                    itemsDesktop: [1700,4],
-                    responsiveBaseWidth: ".featured-properties-carousel",
-                    pagination: false,
-                    startDragging: disableClick,
-                    beforeMove: enableClick
-                });
-                $(".testimonials-carousel").owlCarousel({
-                    items: 1,
-                    responsiveBaseWidth: ".testimonial",
-                    pagination: true
-                });
-                $(".property-carousel").owlCarousel({
-                    items: 1,
-                    responsiveBaseWidth: ".property-slide",
-                    pagination: false,
-                    autoHeight : true,
-                    navigation: true,
-                    navigationText: ["",""],
-                    startDragging: disableClick,
-                    beforeMove: enableClick
-                });
-                $(".homepage-slider").owlCarousel({
-                    autoPlay: 10000,
-                    navigation: true,
-                    mouseDrag: false,
-                    items: 1,
-                    responsiveBaseWidth: ".slide",
-                    pagination: false,
-                    transitionStyle : 'fade',
-                    navigationText: ["",""],
-                    afterInit: sliderLoaded,
-                    afterAction: animateDescription,
-                    startDragging: animateDescription
-                });
-            }
         };
 
         return {
-
-            // Mobile Slider
-
-            centerSlider: centerSlider,
-
-            setNavigationPosition: setNavigationPosition,
-
-            // Agent state - Fired when user change the state if he is agent or doesn't
-
-            agentState: agentState,
-
             initCounter: initCounter,
-
             showAllButton: showAllButton,
-
-            //  Center Search box Vertically
-
-            centerSearchBox: centerSearchBox,
-
-            // Set Owl Carousel width
-
+            setNavigationPosition: setNavigationPosition,
             setCarouselWidth: setCarouselWidth,
-
-            // Show rating form
-
-            showRatingForm: showRatingForm,
-
-            //  Equal heights
-
             equalHeight: equalHeight,
-
+            centerSlider: centerSlider,
             // former windows load
             onStateChange: onStateChange
 
